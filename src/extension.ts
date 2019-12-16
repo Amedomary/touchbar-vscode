75746cef5ea7ca57257ea1d6c1f38695852c4d7d
Max Grishin <amed@ss.com>
2019-12-16 15:34:56 +0300

    add btns

--- src/extension.ts
+++ src/extension.ts
@@ -5,61 +5,6 @@ import * as vscode from "vscode";
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "vs-code-ext-ts" is now active!'
-  );
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  // let disposable = vscode.commands.registerCommand(
-  //   "extension.helloWorld",
-  //   () => {
-  // The code you place here will be executed every time your command is executed
-
-  // Display a message box to the user
-  vscode.window.showInformationMessage("Hello World!");
-
-  let touchbar = [
-    {
-      command: "extension.touchBar.term",
-      action: "workbench.action.terminal.toggleTerminal"
-    },
-    {
-      command: "extension.touchBar.comment",
-      action: "editor.action.commentLine"
-    },
-    {
-      command: "extension.touchBar.qopen",
-      action: "workbench.action.quickOpen"
-    },
-    {
-      command: "extension.touchBar.commands",
-      action: "workbench.action.showCommands"
-    },
-    {
-      command: "extension.touchBar.toggleSidebar",
-      action: "workbench.action.toggleSidebarVisibility"
-    },
-    {
-      command: "extension.touchBar.ftpSync",
-      action: "extension.ftpsyncupload"
-    }
-  ];
-
-  touchbar.forEach(cmd => {
-    // console.log("skipun: " + cmd.command);
-    let command = vscode.commands.registerCommand(cmd.command, () => {
-      vscode.commands.executeCommand(cmd.action);
-    });
-    context.subscriptions.push(command);
-  });
-  // }
-  // );
-
-  // context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
